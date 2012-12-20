9bbee05ac0fafa800b979ed115eb608fa84e0877
TJ Holowaychuk <tj@vision-media.ca>
2012-12-19 16:43:42 -0800

    add username to default template

--- templates/default/index.js
+++ templates/default/index.js
@@ -1,6 +1,7 @@
 
 exports.variables = {
     project: 'Project name: '
+  , username: 'Project username: '
   , name: 'Enter your name: '
   , email: 'Enter your email: '
   , description: 'Project description: '
